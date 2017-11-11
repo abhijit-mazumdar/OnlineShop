6f894dea977594621d78f06f1e1c28a488e34695
pankumazumdar <pankumazumdar@example.com>
2017-11-11 13:59:44 +0530

    Initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyBGIcn0rwkPgmv80RJExjN4IxQUH7GZTUc",
+    authDomain: "oshop-685e5.firebaseapp.com",
+    databaseURL: "https://oshop-685e5.firebaseio.com",
+    projectId: "oshop-685e5",
+    storageBucket: "oshop-685e5.appspot.com",
+    messagingSenderId: "170277764767"
+  }
+};
